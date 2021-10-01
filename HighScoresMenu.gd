2d3ee4fb50458ec28db257259d681e9b10abd97d
svntax <endless.answers@gmail.com>
2021-09-30 20:36:03 -0400

    Added high scores menu with working scores fetching.

---
+++ HighScoresMenu.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+# Note: replace with the name of your deployed smart contract
+const CONTRACT_NAME = "dev-1632885561797-99799145035137"
+
+onready var scores_grid = $ScoresGrid
+onready var player_name_label = $ScoresGrid/PlayerName
+onready var player_score_label = $ScoresGrid/PlayerScore
+
+func _ready():
+	# First, hide the placeholder labels
+	player_name_label.hide()
+	player_score_label.hide()
+	
+	# Next, fetch the high scores and create new labels for each score
+	var result = Near.call_view_method(CONTRACT_NAME, "getScores")
+	if result is GDScriptFunctionState:
+		result = yield(result, "completed")
+	if result.has("error"):
+		pass # Error handling here
+	else:
+		var data = result.data
+		var json_data = JSON.parse(data)
+		var high_scores: Array = json_data.result
+		for score in high_scores:
+			var name_label = player_name_label.duplicate()
+			name_label.set_text(score.username)
+			scores_grid.add_child(name_label)
+			name_label.show()
+			
+			var score_label = player_score_label.duplicate()
+			score_label.set_text(str(score.value))
+			scores_grid.add_child(score_label)
+			score_label.show()
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://MainMenu.tscn")
--- MainMenu.gd
+++ MainMenu.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
+var config = {
+	"network_id": "testnet",
+	"node_url": "https://rpc.testnet.near.org",
+	"wallet_url": "https://wallet.testnet.near.org",
+}
+
+func _ready():
+	if Near.near_connection == null:
+		Near.start_connection(config)
+
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Gameplay.tscn")
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
+
+func _on_HighScoresButton_pressed():
+	get_tree().change_scene("res://HighScoresMenu.tscn")
