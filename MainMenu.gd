2c3932f1b292e1897c5080b7d678a93c27aca81a
svntax <endless.answers@gmail.com>
2021-09-30 23:15:33 -0400

    Added login/logout flow.

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,14 +1,37 @@
 extends Node2D
 
+# Note: replace with the name of your deployed smart contract
+const CONTRACT_NAME = "dev-1632885561797-99799145035137"
+
+onready var login_button = $LoginButton
+onready var player_name_label = $PlayerNameLabel
+
 var config = {
 	"network_id": "testnet",
 	"node_url": "https://rpc.testnet.near.org",
 	"wallet_url": "https://wallet.testnet.near.org",
 }
+var wallet_connection
 
 func _ready():
+	player_name_label.hide()
 	if Near.near_connection == null:
 		Near.start_connection(config)
+	
+	wallet_connection = WalletConnection.new(Near.near_connection)
+	wallet_connection.connect("user_signed_in", self, "_on_user_signed_in")
+	wallet_connection.connect("user_signed_out", self, "_on_user_signed_out")
+	if wallet_connection.is_signed_in():
+		_on_user_signed_in(wallet_connection)
+
+func _on_user_signed_in(wallet: WalletConnection):
+	login_button.set_text("Sign Out")
+	player_name_label.show()
+	player_name_label.set_text(wallet.get_account_id())
+
+func _on_user_signed_out(wallet: WalletConnection):
+	login_button.set_text("Sign In")
+	player_name_label.hide()
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Gameplay.tscn")
@@ -18,3 +41,9 @@ func _on_ExitButton_pressed():
 
 func _on_HighScoresButton_pressed():
 	get_tree().change_scene("res://HighScoresMenu.tscn")
+
+func _on_LoginButton_pressed():
+	if wallet_connection.is_signed_in():
+		wallet_connection.sign_out()
+	else:
+		wallet_connection.sign_in(CONTRACT_NAME)
